05b33b00bcc6b8b07c8ec9cbe638259379105404
nitarte <iii641362@gmail.com>
2025-02-15 12:40:05 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -11,6 +11,87 @@ local teleportConnection = nil
 local inputBeganConnection = nil  
 local humanoid = character:WaitForChild("Humanoid")
 local isSpeedOn = false
+local CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+local speed = 1
+local noclipSpeed = 10
+function startFlying(vflySpeed)
+    if isFlying then return end
+    isFlying = true
+    speed2 = vflySpeed or 50
+    local T = character:WaitForChild("HumanoidRootPart")
+    local BG = Instance.new('BodyGyro')
+    local BV = Instance.new('BodyVelocity')
+    BG.P = 9e4
+    BG.Parent = T
+    BV.Parent = T
+    BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+    BG.cframe = T.CFrame
+    BV.velocity = Vector3.new(0, 0, 0)
+    BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+    task.spawn(function()
+        repeat
+            wait()
+            if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0 then
+                BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + 
+                    ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - 
+                    workspace.CurrentCamera.CoordinateFrame.p)) * speed2
+            else
+                BV.velocity = Vector3.new(0, 0, 0)
+            end
+            BG.cframe = workspace.CurrentCamera.CoordinateFrame
+        until not isFlying
+        BG:Destroy()
+        BV:Destroy()
+    end)
+end
+function stopFlying()
+    isFlying = false
+    humanoid.PlatformStand = false
+end
+function enableNoClip()
+    for _, part in pairs(character:GetDescendants()) do
+        if part:IsA("BasePart") then
+            part.CanCollide = false
+        end
+    end
+end
+function disableNoClip()
+    for _, part in pairs(character:GetDescendants()) do
+        if part:IsA("BasePart") then
+            part.CanCollide = true
+        end
+    end
+end
+UserInputService.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.W then
+        CONTROL.F = noclipSpeed
+    elseif input.KeyCode == Enum.KeyCode.S then
+        CONTROL.B = -noclipSpeed
+    elseif input.KeyCode == Enum.KeyCode.A then
+        CONTROL.L = -noclipSpeed
+    elseif input.KeyCode == Enum.KeyCode.D then
+        CONTROL.R = noclipSpeed
+    elseif input.KeyCode == Enum.KeyCode.Q then
+        CONTROL.Q = noclipSpeed * 2 
+    elseif input.KeyCode == Enum.KeyCode.E then
+        CONTROL.E = -noclipSpeed * 2 
+    end
+end)
+UserInputService.InputEnded:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.W then
+        CONTROL.F = 0
+    elseif input.KeyCode == Enum.KeyCode.S then
+        CONTROL.B = 0
+    elseif input.KeyCode == Enum.KeyCode.A then
+        CONTROL.L = 0
+    elseif input.KeyCode == Enum.KeyCode.D then
+        CONTROL.R = 0
+    elseif input.KeyCode == Enum.KeyCode.Q then
+        CONTROL.Q = 0
+    elseif input.KeyCode == Enum.KeyCode.E then
+        CONTROL.E = 0
+    end
+end)
 local function disableAnimations()
     local animator = humanoid:FindFirstChildOfClass("Animator")
     if animator then
@@ -43,6 +124,8 @@ local function teleportToMouseClick()
         character:SetPrimaryPartCFrame(CFrame.new(clickPosition))
     else
         warn("Слишком далеко для телепортации")
+		info = "Слишком далеко для телепортации"
+		infoMessage(info)
     end
 end
 local function teleportBackToInitialPosition()
@@ -87,9 +170,36 @@ end)
 screenGui.Name = "MyScreenGui"
 screenGui.Enabled = false  
 local teleportMessage = nil  
+local infoMessaget = nil  
 game.Players.PlayerAdded:Connect(function(player)
     addExclamationMarkForServerCreator(player)
 end)
+local function infoMessage(info)
+    if infoMessaget then
+        infoMessaget:Destroy() 
+    end
+    infoMessaget = Instance.new("Frame")
+    infoMessaget.Size = UDim2.new(0, 300, 0, 50)
+    infoMessaget.Position = UDim2.new(1, -310, 0, 10)
+    infoMessaget.BackgroundColor3 = Color3.fromRGB(0, 255, 0) 
+    infoMessaget.BackgroundTransparency = 0.5
+    infoMessaget.BorderSizePixel = 0
+    infoMessaget.Parent = screenGui
+    local message2Label = Instance.new("TextLabel")
+    message2Label.Size = UDim2.new(1, 0, 1, 0)
+    message2Label.Text = info
+    message2Label.BackgroundTransparency = 1
+    message2Label.TextColor3 = Color3.fromRGB(255, 255, 255)  
+    message2Label.Font = Enum.Font.GothamBold
+    message2Label.TextSize = 18
+    message2Label.TextXAlignment = Enum.TextXAlignment.Center
+    message2Label.Parent = infoMessaget
+    delay(7, function()
+        if infoMessaget then
+            infoMessaget:Destroy()
+        end
+    end)
+end
 local function showTeleportMessage(targetPlayer)
     if teleportMessage then
         teleportMessage:Destroy() 
@@ -141,6 +251,8 @@ local function teleportToPlayer(targetPlayer)
         showTeleportMessage(targetPlayer) 
     else
         warn("Ошибка: Персонаж не найден для телепортации")
+		info = "Ошибка: Персонаж не найден"
+		infoMessage(info)
     end
 end
 local function calculateDistance(player1, player2)
@@ -228,6 +340,8 @@ local function addPlayerButtons()
                     end
                 else
                     warn("Ошибка: Персонаж не найден")
+					info = "Ошибка: Персонаж не найден"
+					infoMessage(info)
                 end
             end)
             local billboardGui = Instance.new("BillboardGui")
@@ -299,7 +413,7 @@ clearGlowButton.TextSize = 16
 clearGlowButton.Parent = frame
 local tpbut = Instance.new("TextButton")
 tpbut.Size = UDim2.new(0, 150, 0, 30)
-tpbut.Position = UDim2.new(0, 155, 0, -20)
+tpbut.Position = UDim2.new(0, 160, 0, -20)
 tpbut.Text = "Teleport on"
 tpbut.BackgroundColor3 = Color3.fromRGB(255, 165, 0)
 tpbut.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -316,10 +430,14 @@ local function toggletp()
         tpbut.Text = "Teleport on"
         tpbut.BackgroundColor3 = Color3.fromRGB(255, 165, 0)
         statustelep = false
+		info = "Телепортации включены!"
+		infoMessage(info)
     else
         tpbut.Text = "Teleport off"
         tpbut.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
-        statustelep = true  -- Отключаем телепортацию
+        statustelep = true  
+		info = "Телепортации выключены!"
+		infoMessage(info)
     end
     istpOn = not istpOn
     teleportyes(statustelep)
@@ -353,8 +471,12 @@ inputBox.FocusLost:Connect(function()
     local speedValue = tonumber(inputBox.Text)
     if speedValue and speedValue > 0 then
         humanoid.WalkSpeed = speedValue
+		info = "Скорость установленна на " .. speedValue
+		infoMessage(info)
     else
         warn("Неверное значение скорости")
+		info = "Значение не может быть меньше 0!"
+		infoMessage(info)
     end
 end)
 local function toggleSpeed()
@@ -362,17 +484,77 @@ local function toggleSpeed()
         speedbut.Text = "Speed on"
         speedbut.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
         humanoid.WalkSpeed = 16
-        humanoid.JumpPower = 50 
+        humanoid.JumpPower = 50
+		info = "Супер скорость включенна!"
+		infoMessage(info)
     else
         speedbut.Text = "Speed off"
         speedbut.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
         instantSpeedBoost()
         applyBodyVelocity()
+		info = "Супер скорость отключенна!"
+		infoMessage(info)
     end
     isSpeedOn = not isSpeedOn 
 end
 speedbut.MouseButton1Click:Connect(function()
     toggleSpeed()  
+end)
+local nbut = Instance.new("TextButton")
+nbut.Size = UDim2.new(0, 150, 0, 30) 
+nbut.Position = UDim2.new(0, 320, 0, -20) 
+nbut.Text = "Noclip on"
+nbut.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+nbut.TextColor3 = Color3.fromRGB(255, 255, 255)
+nbut.Font = Enum.Font.Gotham
+nbut.TextSize = 16
+nbut.TextXAlignment = Enum.TextXAlignment.Center
+nbut.Parent = frame
+local isFlyAndNoClipOn = false 
+local function toggleFlyAndNoClip()
+    if isFlyAndNoClipOn then
+        stopFlying()
+        disableNoClip()
+        nbut.Text = "NoClip On"
+		info = "Ноуклип выключен!"
+		infoMessage(info)
+        nbut.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+    else
+        startFlying(noclipSpeed)
+        enableNoClip()
+        nbut.Text = "NoClip Off"
+		info = "Ноуклип включен!"
+		infoMessage(info)
+        nbut.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+    end
+    isFlyAndNoClipOn = not isFlyAndNoClipOn
+end
+nbut.MouseButton1Click:Connect(function()
+    toggleFlyAndNoClip()
+end)
+local inputBox = Instance.new("TextBox")
+inputBox.Size = UDim2.new(0, 150, 0, 30)
+inputBox.Position = UDim2.new(0, 320, 0, -60)
+inputBox.PlaceholderText = "ВВЕДИ СКОРОСТЬ"
+inputBox.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+inputBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+inputBox.Font = Enum.Font.Gotham
+inputBox.TextSize = 16
+inputBox.Parent = frame
+inputBox.FocusLost:Connect(function()
+    local speedValue = tonumber(inputBox.Text)
+    if speedValue and speedValue > 0 then
+        noclipSpeed = speedValue 
+        if isFlyAndNoClipOn then
+            startFlying(noclipSpeed)
+			info = "Скорость установленна на " .. noclipSpeed
+			infoMessage(info)
+        end
+    else
+        warn("Invalid speed value")
+		info = "Значение не может быть меньше 0!"
+		infoMessage(info)
+    end
 end)
 local function createFlagEffect(character)
     local whiteFlag = Instance.new("Part")
@@ -382,7 +564,7 @@ local function createFlagEffect(character)
     whiteFlag.Anchored = true
     whiteFlag.Parent = game.Workspace
     local blueFlag = Instance.new("Part")
-    blueFlag.Size = Vector3.new(50, 20, 0.20)  -- Размер флага
+    blueFlag.Size = Vector3.new(50, 20, 0.20)
     blueFlag.Position = whiteFlag.Position - Vector3.new(0, 20.1, 0)  
     blueFlag.BrickColor = BrickColor.new(Color3.fromRGB(0, 0, 255)) 
     blueFlag.Anchored = true
@@ -397,8 +579,9 @@ end
 clearGlowButton.MouseButton1Click:Connect(function()
 	createFlagEffect(player.Character)
 	disableAnimations()
+	info = "Вьебал русской любви!"
+	infoMessage(info)
 end)
-
 clearGlowButton.MouseButton1Click:Connect(function()
     for _, selectionBox in ipairs(activeSelectionBoxes) do
         selectionBox:Destroy()
@@ -425,6 +608,8 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
                 teleportToPlayer(lastTeleportedPlayer)
             else
                 warn("Нет последнего телепортированного игрока.")
+				info = "Нет последнего телепортированного игрока!"
+				infoMessage(info)
             end
         end
     end
